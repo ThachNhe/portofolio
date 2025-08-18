a64020a44fe2f2f90d9e538bd03b0e89e2e3629b
thachdv <thachdv.it.dev@gmail.com>
2025-08-18 22:06:09 +0700

    initial portofolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-8 bg-red-800">
+      <h1 className="text-3xl font-bold underline text-red-800">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
